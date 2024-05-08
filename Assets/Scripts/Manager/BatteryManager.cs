201b93429f847c00c5e38d6831450f0fcbc5c6c3
방민호 <100828741+bamin0502@users.noreply.github.com>
2024-05-08 13:51:07 +0900

    초기 설정 구성

---
+++ Assets/Scripts/Manager/BatteryManager.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Collections;
+using UnityEngine;
+
+public class BatteryManager : MonoBehaviour
+{
+    public static BatteryManager Instance;
+
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void Start()
+    {
+        StartCoroutine(CheckBatteryStatus());
+    }
+
+    private IEnumerator CheckBatteryStatus()
+    {
+        while (true)
+        {
+            AdjustSettingsBasedOnBattery(SystemInfo.batteryStatus, SystemInfo.batteryLevel);
+            yield return new WaitForSeconds(60);
+        }
+    }
+
+    private void AdjustSettingsBasedOnBattery(BatteryStatus status, float level)
+    {
+        if (status is BatteryStatus.Charging or BatteryStatus.Full)
+        {
+            SetGameSettings(60, 1, 5);
+        }
+        else if (status == BatteryStatus.Discharging)
+        {
+            if (level < 0.2f)
+            {
+                SetGameSettings(15, 0, 1);
+            }
+            else
+            {
+                SetGameSettings(30, 0, 3);
+            }
+        }
+        else
+        {
+            SetGameSettings(30, 0, 3);
+        }
+    }
+
+    private void SetGameSettings(int frameRate, int vSyncCount, int qualityLevel)
+    {
+        Application.targetFrameRate = frameRate;
+        QualitySettings.vSyncCount = vSyncCount;
+        QualitySettings.SetQualityLevel(qualityLevel);
+    }
+}
