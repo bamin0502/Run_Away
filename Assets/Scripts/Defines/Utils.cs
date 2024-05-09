ed0e7fc40c3b6d7a792105d59d3c7c0738fe8dec
방민호 <100828741+bamin0502@users.noreply.github.com>
2024-05-09 11:59:26 +0900

    스와이프 액션 관련 일부 수정

--- Assets/Scripts/Defines/Utils.cs
+++ Assets/Scripts/Defines/Utils.cs
@@ -4,11 +4,7 @@ public class Utils : MonoBehaviour
 {
     public static Vector3 ScreenToWorld(Camera camera, Vector3 screenPosition)
     {
-        Ray ray = camera.ScreenPointToRay(screenPosition);
-        if (Physics.Raycast(ray, out RaycastHit hit))
-        {
-            return hit.point;
-        }
-        return Vector3.zero;
+        var ray = camera.ScreenPointToRay(screenPosition);
+        return Physics.Raycast(ray, out RaycastHit hit) ? hit.point : Vector3.zero;
     }
 }
--- Assets/Scripts/Player/SwipeDetection.cs
+++ Assets/Scripts/Player/SwipeDetection.cs
@@ -3,102 +3,78 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody))]
 public class SwipeDetection : MonoBehaviour
 {
-    [Tooltip("최소 스와이프 거리"), Header("Swipe Settings")]
-    [SerializeField] private float minSwipeDistance = 0.2f;  // 최소 스와이프 거리 설정
+    [SerializeField] private float minSwipeDistance = 0.2f;
     [SerializeField, Range(0f, 1f)] private float directionThreshold = 0.7f;
-    
+
     private InputManager inputManager;
     private Vector2 startPosition;
     private Vector2 endPosition;
     private float startTime;
     private float endTime;
-    
+
     private Rigidbody rb;
-    private Vector3 pendingMove;
-    private bool shouldMove;
+    private Vector3 moveAmount;
     
     private void Awake()
     {
         inputManager = InputManager.Instance;
         rb = GetComponent<Rigidbody>();
     }
+
     private void OnEnable()
     {
         inputManager.OnStartTouch += SwipeStart;
         inputManager.OnEndTouch += SwipeEnd;
     }
+
     private void OnDisable()
     {
         inputManager.OnStartTouch -= SwipeStart;
         inputManager.OnEndTouch -= SwipeEnd;
     }
-    
+
     private void SwipeStart(Vector2 position, float time)
     {
         startPosition = position;
         startTime = time;
     }
+
     private void SwipeEnd(Vector2 position, float time)
     {
         endPosition = position;
         endTime = time;
-        
+
         DetectSwipe();
     }
-    
+
     private void DetectSwipe()
     {
-        if (Vector3.Distance(startPosition, endPosition) >= minSwipeDistance && (endTime - startTime) < 0.5f)
+        float distance = Vector2.Distance(startPosition, endPosition);
+        if (distance >= minSwipeDistance && (endTime - startTime) < 0.5f)
         {
-            Vector2 direction = endPosition - startPosition;
-            Vector2 normalizedDirection = direction.normalized;
-            
-            if (Vector2.Dot(Vector2.up, normalizedDirection) > directionThreshold)
-            {
-                pendingMove = Vector3.up;
-                Debug.Log("Up");
-                shouldMove = true;
-            }
-            else if (Vector2.Dot(Vector2.down, normalizedDirection) > directionThreshold)
-            {
-                pendingMove = Vector3.down;
-                Debug.Log("Down");
-                shouldMove = true;
-            }
-            else if (Vector2.Dot(Vector2.right, normalizedDirection) > directionThreshold)
-            {
-                pendingMove = Vector3.right;
-                Debug.Log("Right");
-                shouldMove = true;
-            }
-            else if (Vector2.Dot(Vector2.left, normalizedDirection) > directionThreshold)
-            {
-                pendingMove = Vector3.left;
-                Debug.Log("Left");
-                shouldMove = true;
-            }
+            Vector2 direction = (endPosition - startPosition).normalized;
+            UpdateMovement(direction);
         }
     }
-    
+
+    private void UpdateMovement(Vector2 direction)
+    {
+        float horizontalDot = Vector2.Dot(Vector2.right, direction);
+        float verticalDot = Vector2.Dot(Vector2.up, direction);
+
+        if (Mathf.Abs(horizontalDot) > directionThreshold)
+        {
+            moveAmount.x += (horizontalDot > 0 ? 1 : -1) * 5;  // 오른쪽 또는 왼쪽으로 5단위 이동
+        }
+        if (Mathf.Abs(verticalDot) > directionThreshold)
+        {
+            moveAmount.y += (verticalDot > 0 ? 1 : -1) * 0.5f;  // 위 또는 아래로 0.5단위 이동
+        }
+    }
+
     private void FixedUpdate()
     {
-        if (shouldMove)
-        {
-            rb.MovePosition(rb.position + pendingMove);
-            shouldMove = false;
-        }
+        rb.MovePosition(rb.position + moveAmount); 
+        moveAmount = Vector3.zero; 
     }
-    
-    private void OnDrawGizmos()
-    {
-        Gizmos.color = Color.red;
-        Gizmos.DrawLine(startPosition, endPosition);
-    }
-    
-    private void OnGUI()
-    {
-        GUI.Label(new Rect(0, 0, 100, 100), $"Start: {startPosition}\nEnd: {endPosition}");
-    }
-    
-    
 }
