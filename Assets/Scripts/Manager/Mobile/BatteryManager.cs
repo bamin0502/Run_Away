beb4719eaae404ab2a3ad1ccf0a47870d20783eb
방민호 <100828741+bamin0502@users.noreply.github.com>
2024-05-28 16:03:08 +0900

    배터리 매니저(코루틴 -> UniRx로 변경) , 게임씬 UI 레이아웃 이상한거 변경

--- Assets/Scripts/Manager/Mobile/BatteryManager.cs
+++ Assets/Scripts/Manager/Mobile/BatteryManager.cs
@@ -1,38 +1,48 @@
-using System.Collections;
+using System; 
 using UnityEngine;
 using UniRx;
 
 public class BatteryManager : Singleton<BatteryManager>
 {
+    private CompositeDisposable disposables = new CompositeDisposable();
+
     private void Start()
     {
-        StartCoroutine(CheckBatteryStatus());
+        CheckBatteryStatus();
     }
+    
     private void OnApplicationPause(bool pauseStatus)
     {
         if (pauseStatus)
         {
             SetGameSettings(5, 0, 1);
+            disposables.Clear();
         }
         else
         {
-            StartCoroutine(CheckBatteryStatus());
+            CheckBatteryStatus();
         }
     }
-    private IEnumerator CheckBatteryStatus()
+
+    private void OnDestroy()
     {
-        while (true)
-        {
-            AdjustSettingsBasedOnBattery(SystemInfo.batteryStatus, SystemInfo.batteryLevel);
-            yield return new WaitForSeconds(60);
-        }
+        disposables.Dispose();
+    }
+
+    private void CheckBatteryStatus()
+    {
+        Observable.Interval(TimeSpan.FromSeconds(60))
+            .StartWith(0)
+            .Subscribe(_ => AdjustSettingsBasedOnBattery(SystemInfo.batteryStatus, SystemInfo.batteryLevel))
+            .AddTo(disposables);
     }
 
     private void AdjustSettingsBasedOnBattery(BatteryStatus status, float level)
     {
         switch (status)
         {
-            case BatteryStatus.Charging or BatteryStatus.Full:
+            case BatteryStatus.Charging:
+            case BatteryStatus.Full:
                 SetGameSettings(60, 1, 5);
                 break;
             case BatteryStatus.Discharging when level < 0.2f:
